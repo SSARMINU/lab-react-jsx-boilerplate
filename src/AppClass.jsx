3b30b9dd11f3f72859e33ac0be8dff5850692995
SSARMINU <127767691+SSARMINU@users.noreply.github.com>
2024-05-23 20:29:04 +0530

    Update AppClass.jsx

--- src/AppClass.jsx
+++ src/AppClass.jsx
@@ -1,41 +1,30 @@
-import { Component } from "react";
-import "./App.css"
-import elephant from "./images/elephant.jpeg";
+import React, { Component } from 'react';
+import './App.css';
+import elephant from "./images/elephant.jpeg";  // Ensure this path is correct
 
-export default class AppClass extends Component{
-  
-  imageData = ()=>{
-    let data = [
-      {
-        id:1,
-        img:elephant
-      },
-      {
-        id:2,
-        img:elephant
-      },
-      {
-        id:3,
-        img:elephant
-      },
-      {
-        id:4,
-        img:elephant
-      }
-    ]
-    return data;
-  }
+const imageData = () => {
+  return [
+    { id: 1, img: elephant },
+    { id: 2, img: elephant },
+    { id: 3, img: elephant },
+    { id: 4, img: elephant },
+  ];
+}
 
+class AppClass extends Component {
   render() {
+    const images = imageData();
+
     return (
-      <div className="App">
-        <h1>Elephant Images</h1>
-        <div className="image-container">
-          {this.imageData().map((item) => (
-            <img key={item.id} src={item.img} alt={`Elephant ${item.id}`} />
-          ))}
-        </div>
+      <div className="image-gallery">
+        {images.map((img) => (
+          <div key={img.id} className="image-container">
+            <img src={img.img} alt={`img-${img.id}`} className="image" />
+          </div>
+        ))}
       </div>
     );
   }
 }
+
+export default AppClass;
